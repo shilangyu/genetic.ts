2554aa9129b28513a8e77e36448b28b037576535
Marcin Wojnarowski <xmarcinmarcin@gmail.com>
2018-11-07 20:33:27 +0100

    [overhaul] created a static method validatePopulation

--- Genetic.ts
+++ Genetic.ts
@@ -9,7 +9,7 @@ export const enum CrossoverModes {
 	clone = 'CLONE'
 }
 
-type DNA = { [key: string]: number }
+type DNA = any
 
 interface IPopMember {
 	fitness: number
@@ -93,5 +93,60 @@ export default class Genetic {
 		}
 
 		return this
+	}
+
+	static validatePopulation(population: any[]) {
+		if (!population.every(mem => 'fitness' in mem))
+			throw new Error('Member of the population is missing the fitness property.')
+
+		if (!population.every(mem => typeof mem.fitness === 'number'))
+			throw new Error('Fitness of a member of the population is not of type number.')
+
+		if (!population.every(mem => 'dna' in mem))
+			throw new Error('Member of the population is missing the dna property.')
+
+		function validateTypes(obj: any) {
+			if (Array.isArray(obj)) for (const ele of obj) validateTypes(ele)
+			else if (typeof obj === 'object') for (const ele of Object.values(obj)) validateTypes(ele)
+			else if (typeof obj !== 'number')
+				throw new Error('Dna of a member of the population has an incorrect type.')
+		}
+		validateTypes(population)
+
+		const zip = (a: any, b: any): any[] => a.map((e: any, i: number) => [e, b[i]])
+
+		function validateStructure(obj: any, model: any) {
+			if (Array.isArray(model)) {
+				if (obj.length !== model.length)
+					throw new Error('Dna of a member of the population has a different structure.')
+
+				for (const [a, b] of zip(obj, model)) {
+					if (Array.isArray(a) !== Array.isArray(b)) {
+						throw new Error('Dna of a member of the population has a different structure.')
+					} else if (typeof a !== typeof b) {
+						throw new Error('Dna of a member of the population has a different structure.')
+					} else if (typeof a !== 'number') {
+						validateStructure(a, b)
+					}
+				}
+			} else if (typeof obj === 'object') {
+				if (!zip(Object.keys(obj), Object.keys(model)).every(([a, b]) => a === b)) {
+					throw new Error('Dna of a member of the population has a different structure.')
+				}
+
+				for (const [a, b] of zip(Object.values(obj), Object.values(model))) {
+					if (Array.isArray(a) !== Array.isArray(b)) {
+						throw new Error('Dna of a member of the population has a different structure.')
+					} else if (typeof a !== typeof b) {
+						throw new Error('Dna of a member of the population has a different structure.')
+					} else if (typeof a !== 'number') {
+						validateStructure(a, b)
+					}
+				}
+			} else if (typeof obj !== 'number')
+				throw new Error('Dna of a member of the population has a different structure.')
+		}
+
+		for (const curr of population.slice(1)) validateStructure(curr, population[0])
 	}
 }
---
+++ tests/validatePopulation.test.ts
@@ -0,0 +1,130 @@
+import Genetic from '../Genetic'
+
+describe('`validatePopulation` static method of the Genetic class', () => {
+	it('tests missing fitness', () => {
+		const mockPopulation = [
+			{ fitness: 100, dna: { asd: 1 } },
+			{ dna: { asd: 2 } },
+			{ fitness: 300, dna: { asd: 3 } },
+			{ fitness: 400, dna: { asd: 4 } },
+			{ fitness: 500, dna: { asd: 5 } }
+		]
+
+		const expected = 'Member of the population is missing the fitness property.'
+		let result = ''
+
+		try {
+			Genetic.validatePopulation(mockPopulation)
+		} catch (err) {
+			result = err.message
+		}
+
+		expect(result).toBe(expected)
+	})
+
+	it('tests wrong type fitness', () => {
+		const mockPopulation = [
+			{ fitness: 100, dna: { asd: 1 } },
+			{ fitness: 'a', dna: { asd: 2 } },
+			{ fitness: 300, dna: { asd: 3 } },
+			{ fitness: 400, dna: { asd: 4 } },
+			{ fitness: 500, dna: { asd: 5 } }
+		]
+
+		const expected = 'Fitness of a member of the population is not of type number.'
+		let result = ''
+
+		try {
+			Genetic.validatePopulation(mockPopulation)
+		} catch (err) {
+			result = err.message
+		}
+
+		expect(result).toBe(expected)
+	})
+
+	it('tests missing DNA', () => {
+		const mockPopulation = [
+			{ fitness: 100, dna: { asd: 1 } },
+			{ fitness: 200, dna: { asd: 2 } },
+			{ fitness: 300, dna: { asd: 3 } },
+			{ fitness: 400 },
+			{ fitness: 500, dna: { asd: 5 } }
+		]
+
+		const expected = 'Member of the population is missing the dna property.'
+		let result = ''
+
+		try {
+			Genetic.validatePopulation(mockPopulation)
+		} catch (err) {
+			result = err.message
+		}
+
+		expect(result).toBe(expected)
+	})
+
+
+	it('tests wrong type DNA', () => {
+		const mockPopulation = [
+			{ fitness: 100, dna: { asd: { abc: [1, 2, [0]] } } },
+			{ fitness: 200, dna: { asd: { abc: [1, 2, [0]] } } },
+			{ fitness: 300, dna: { asd: { abc: [1, 2, ['1']] } } },
+			{ fitness: 400, dna: { asd: { abc: [1, 2, [0]] } } },
+			{ fitness: 500, dna: { asd: { abc: [1, 2, [0]] } } }
+		]
+
+		const expected = 'Dna of a member of the population has an incorrect type.'
+		let result = ''
+
+		try {
+			Genetic.validatePopulation(mockPopulation)
+		} catch (err) {
+			result = err.message
+		}
+
+		expect(result).toBe(expected)
+	})
+
+	it('tests inconsistent dna throughout the population', () => {
+		const mockPopulation = [
+			{ fitness: 100, dna: { asd: { abc: [1, 2, [0]] } } },
+			{ fitness: 200, dna: { asd: { abc: [1, 2, [0]] } } },
+			{ fitness: 300, dna: { asd: { azc: [1, 2, [0]] } } },
+			{ fitness: 400, dna: { asd: { abc: [1, 2, [0]] } } },
+			{ fitness: 500, dna: { asd: { abc: [1, 2, [0]] } } }
+		]
+
+		const expected = 'Dna of a member of the population has a different structure.'
+		let result = ''
+
+		try {
+			Genetic.validatePopulation(mockPopulation)
+		} catch (err) {
+			result = err.message
+		}
+
+		expect(result).toBe(expected)
+	})
+
+	it('tests a valid population', () => {
+		const mockPopulation = [
+			{ fitness: 100, dna: { asd: { abc: [1, 2, [0]] } } },
+			{ fitness: 200, dna: { asd: { abc: [1, 2, [0]] } } },
+			{ fitness: 300, dna: { asd: { abc: [1, 2, [123]] } } },
+			{ fitness: 400, dna: { asd: { abc: [1, 2, [0]] } } },
+			{ fitness: 500, dna: { asd: { abc: [1, 2, [0]] } } }
+		]
+
+		const expected = ''
+		let result = ''
+
+		try {
+			Genetic.validatePopulation(mockPopulation)
+		} catch (err) {
+			result = err.message
+		}
+
+		expect(result).toBe(expected)
+	})
+})
