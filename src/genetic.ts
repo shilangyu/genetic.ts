fdfe5d76d7eda9e67b896da98526204c30debf53
shilangyu <xmarcinmarcin@gmail.com>
2019-12-09 16:52:27 +0100

    split into files

--- src/genetic.ts
+++ src/genetic.ts
@@ -1,3 +1,5 @@
+import { dnaCopy } from './util'
+
 export enum ParentsSelectionModes {
   best,
   probability
@@ -28,7 +30,7 @@ export interface IPopMember {
   dna: DNA
 }
 
-type MutationFunction = (mutationRate: number) => number
+export type MutationFunction = (mutationRate: number) => number
 type MapDnaFunction<T> = (newDna: DNA[]) => T[]
 
 export const Instance = class<Member extends IPopMember>
@@ -195,36 +197,24 @@ export const Instance = class<Member extends IPopMember>
   }
 
   mutate(): this {
-    const deeper = (target: any[]): any => {
+    const deep = (target: DNA): any => {
       if (Array.isArray(target)) {
-        return target.map(deeper)
+        return target.map(deep)
       } else if (typeof target === 'object') {
         const temp: any = {}
         for (const key of Object.keys(target)) {
-          temp[key] = deeper(target[key])
+          temp[key] = deep(target[key])
         }
         return temp
       } else if (typeof target === 'number')
         return target + this.mutationFunction(this.mutationRate)
     }
 
-    const copy = (target: any[]): any => {
-      if (Array.isArray(target)) {
-        return target.map(deeper)
-      } else if (typeof target === 'object') {
-        const temp: any = {}
-        for (const key of Object.keys(target)) {
-          temp[key] = deeper(target[key])
-        }
-        return temp
-      } else if (typeof target === 'number') return target
-    }
-
     for (let i = 0; i < this.newDna.length; i++) {
       if (this.preserveParents && i < this.numberOfParents) {
-        this.newDna[i] = copy(this.parents[i])
+        this.newDna[i] = dnaCopy(this.parents[i])
       } else {
-        this.newDna[i] = deeper(this.newDna[i])
+        this.newDna[i] = deep(this.newDna[i])
       }
     }
 
@@ -246,97 +236,5 @@ export const Instance = class<Member extends IPopMember>
   }
 }
 
-export const validatePopulation = (population: any) => {
-  if (!Array.isArray(population))
-    throw new Error('The population is not an array.')
-
-  if (
-    !population.every(
-      mem => 'fitness' in mem && typeof mem.fitness === 'function'
-    )
-  )
-    throw new Error('Member of the population is missing the fitness method.')
-
-  if (!population.every(mem => typeof mem.fitness() === 'number'))
-    throw new Error(
-      'Fitness of a member of the population returns something else than a number.'
-    )
-
-  if (!population.every(mem => 'dna' in mem))
-    throw new Error('Member of the population is missing the dna property.')
-
-  function validateDnaTypes(obj: any) {
-    if (Array.isArray(obj)) for (const ele of obj) validateDnaTypes(ele)
-    else if (typeof obj === 'object')
-      for (const ele of Object.values(obj)) validateDnaTypes(ele)
-    else if (typeof obj !== 'number')
-      throw new Error(
-        'Dna of a member of the population has an incorrect type.'
-      )
-  }
-  validateDnaTypes(population.map(p => p.dna))
-
-  const zip = (a: any, b: any): any[] => a.map((e: any, i: number) => [e, b[i]])
-
-  function validateStructure(obj: any, model: any) {
-    if (Array.isArray(model)) {
-      if (obj.length !== model.length)
-        throw new Error(
-          'Dna of a member of the population has a different structure.'
-        )
-
-      for (const [a, b] of zip(obj, model)) {
-        if (Array.isArray(a) !== Array.isArray(b)) {
-          throw new Error(
-            'Dna of a member of the population has a different structure.'
-          )
-        } else if (typeof a !== typeof b) {
-          throw new Error(
-            'Dna of a member of the population has a different structure.'
-          )
-        } else if (typeof a !== 'number') {
-          validateStructure(a, b)
-        }
-      }
-    } else if (typeof obj === 'object') {
-      if (
-        !zip(Object.keys(obj), Object.keys(model)).every(([a, b]) => a === b)
-      ) {
-        throw new Error(
-          'Dna of a member of the population has a different structure.'
-        )
-      }
-
-      for (const [a, b] of zip(Object.values(obj), Object.values(model))) {
-        if (Array.isArray(a) !== Array.isArray(b)) {
-          throw new Error(
-            'Dna of a member of the population has a different structure.'
-          )
-        } else if (typeof a !== typeof b) {
-          throw new Error(
-            'Dna of a member of the population has a different structure.'
-          )
-        } else if (typeof a !== 'number') {
-          validateStructure(a, b)
-        }
-      }
-    } else if (typeof obj !== 'number')
-      throw new Error(
-        'Dna of a member of the population has a different structure.'
-      )
-  }
-
-  for (const curr of population.slice(1))
-    validateStructure(curr.dna, population[0].dna)
-}
-
-export const chance = (
-  func: MutationFunction
-): MutationFunction => mutationRate => {
-  if (Math.random() < mutationRate) return func(mutationRate)
-  return 0
-}
-
-export const add = (min: number, max: number): MutationFunction => () => {
-  return Math.random() * (max - min) + min
-}
+export { add, chance } from './mutators'
+export { validatePopulation } from './util'
